727c3d4a6c1e27966b0c17cd8db5ae6b15bd27b6
Maggie Sun <167135003+maggiesun412@users.noreply.github.com>
2024-07-12 18:27:06 -0400

    Add files via upload

--- Covid_data_ssms.sql
+++ Covid_data_ssms.sql
@@ -19,80 +19,9 @@ SELECT location, date, total_cases, new_cases, population
 FROM CovidData..CovidDeaths
 ORDER BY 1, 2
 
--- Update column types
-
-ALTER TABLE CovidDeaths ALTER COLUMN total_deaths int
-ALTER TABLE CovidDeaths ALTER COLUMN total_cases int
-ALTER TABLE CovidDeaths ALTER COLUMN population bigint
-ALTER TABLE CovidDeaths ALTER COLUMN new_cases int
-ALTER TABLE CovidDeaths ALTER COLUMN new_cases_smoothed float
-ALTER TABLE CovidDeaths ALTER COLUMN new_deaths bigint
-ALTER TABLE CovidDeaths ALTER COLUMN new_deaths_smoothed float
-ALTER TABLE CovidDeaths ALTER COLUMN total_cases_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN new_cases_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN new_cases_smoothed_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN total_deaths_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN new_deaths_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN new_deaths_smoothed_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN reproduction_rate float
-ALTER TABLE CovidDeaths ALTER COLUMN icu_patients int
-ALTER TABLE CovidDeaths ALTER COLUMN icu_patients_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN hosp_patients int
-ALTER TABLE CovidDeaths ALTER COLUMN hosp_patients_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN weekly_icu_admissions int
-ALTER TABLE CovidDeaths ALTER COLUMN weekly_icu_admissions_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN weekly_hosp_admissions int
-ALTER TABLE CovidDeaths ALTER COLUMN weekly_hosp_admissions_per_million float
-ALTER TABLE CovidDeaths ALTER COLUMN total_tests bigint
-
-ALTER TABLE CovidVaccinations ALTER COLUMN new_tests int
-ALTER TABLE CovidVaccinations ALTER COLUMN total_tests_per_thousand float
-ALTER TABLE CovidVaccinations ALTER COLUMN new_tests_per_thousand float
-ALTER TABLE CovidVaccinations ALTER COLUMN new_tests_smoothed int
-ALTER TABLE CovidVaccinations ALTER COLUMN new_tests_smoothed_per_thousand float
-ALTER TABLE CovidVaccinations ALTER COLUMN positive_rate float
-ALTER TABLE CovidVaccinations ALTER COLUMN tests_per_case float
-ALTER TABLE CovidVaccinations ALTER COLUMN total_vaccinations bigint
-ALTER TABLE CovidVaccinations ALTER COLUMN people_vaccinated bigint
-ALTER TABLE CovidVaccinations ALTER COLUMN people_fully_vaccinated bigint
-ALTER TABLE CovidVaccinations ALTER COLUMN total_boosters bigint
-ALTER TABLE CovidVaccinations ALTER COLUMN new_vaccinations int
-ALTER TABLE CovidVaccinations ALTER COLUMN new_vaccinations_smoothed int
-ALTER TABLE CovidVaccinations ALTER COLUMN total_vaccinations_per_hundred float
-ALTER TABLE CovidVaccinations ALTER COLUMN people_vaccinated_per_hundred float
-ALTER TABLE CovidVaccinations ALTER COLUMN people_fully_vaccinated_per_hundred float
-ALTER TABLE CovidVaccinations ALTER COLUMN total_boosters_per_hundred float
-ALTER TABLE CovidVaccinations ALTER COLUMN new_vaccinations_smoothed_per_million int
-ALTER TABLE CovidVaccinations ALTER COLUMN new_people_vaccinated_smoothed int
-ALTER TABLE CovidVaccinations ALTER COLUMN new_people_vaccinated_smoothed_per_hundred float
-ALTER TABLE CovidVaccinations ALTER COLUMN stringency_index float
-ALTER TABLE CovidVaccinations ALTER COLUMN population_density float
-ALTER TABLE CovidVaccinations ALTER COLUMN median_age float
-ALTER TABLE CovidVaccinations ALTER COLUMN aged_65_older float
-ALTER TABLE CovidVaccinations ALTER COLUMN aged_70_older float
-ALTER TABLE CovidVaccinations ALTER COLUMN gdp_per_capita float
-ALTER TABLE CovidVaccinations ALTER COLUMN extreme_poverty float
-ALTER TABLE CovidVaccinations ALTER COLUMN cardiovasc_death_rate float
-ALTER TABLE CovidVaccinations ALTER COLUMN diabetes_prevalence float
-ALTER TABLE CovidVaccinations ALTER COLUMN female_smokers float
-ALTER TABLE CovidVaccinations ALTER COLUMN male_smokers float
-ALTER TABLE CovidVaccinations ALTER COLUMN handwashing_facilities float
-ALTER TABLE CovidVaccinations ALTER COLUMN hospital_beds_per_thousand float
-ALTER TABLE CovidVaccinations ALTER COLUMN life_expectancy float
-ALTER TABLE CovidVaccinations ALTER COLUMN human_development_index float
-ALTER TABLE CovidVaccinations ALTER COLUMN excess_mortality_cumulative_absolute float
-ALTER TABLE CovidVaccinations ALTER COLUMN excess_mortality_cumulative float
-ALTER TABLE CovidVaccinations ALTER COLUMN excess_mortality float
-ALTER TABLE CovidVaccinations ALTER COLUMN excess_mortality_cumulative_per_million float
-
-ALTER TABLE CovidDeaths
-ALTER COLUMN date DATETIME
-
-ALTER TABLE CovidVaccinations
-ALTER COLUMN date DATETIME
 
 
--- Compaing Total Cases vs Total Deaths
+-- Comparing Total Cases vs Total Deaths
 -- Shows likelihood of dying if you contract covid in your country
 
 
